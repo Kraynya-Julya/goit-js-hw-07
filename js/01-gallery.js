456d5539bc482b52b48fc9ffb6fc1562d344d88c
Крайняя Юлия <kraynya1991@gmail.com>
2022-02-01 16:17:29 +0200

    7-1

--- js/01-gallery.js
+++ js/01-gallery.js
@@ -1,35 +1,29 @@
-import { galleryItems } from './gallery-items.js';
-// Change code below this line
-const galleryContainer = document.querySelector('.gallary');
+const galleryContainer = document.querySelector('.gallery');
 const cardsGallery = createImagesCardsGallery(galleryItems);
 // console.log(galleryItems);
-
 galleryContainer.insertAdjacentHTML('beforeend', cardsGallery);
 galleryContainer.addEventListener('click', onPaletteContainerClick);
-
 function createImagesCardsGallery(galleryItems) {
-    return galleryItems.map(({original, preview, description}) =>{
+    return galleryItems.map(({original, preview, description}) => {
         return `
         <div class="gallery__item">
-        <a class="gallery__link" href="${original}">
-          <img
-            class="gallery__image"
-            src="${preview}"
-            data-source="${original}"
-            alt="${description}"
-          />
-        </a>
-      </div>`}).join('');
+            <a class="gallery__link" href="${original}">
+                <img
+                    class="gallery__image"
+                    src="${preview}"
+                    data-source="${original}"
+                    alt="${description}"
+                />
+            </a>
+      </div>`
+    }).join('');
 }
-
 function onPaletteContainerClick(event) {
-    event.preventDefault() 
+    event.preventDefault();
     const isImageEl = event.target.classList.contains('gallery__image');
-     
     if(!isImageEl) {
         return;
     }
-
-const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`);
-instance.show();
+    const instance = basicLightbox.create(`<img src="${event.target.dataset.source}">`)
+    instance.show();
 }
